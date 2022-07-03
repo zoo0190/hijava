7df4f7c3ff64d1c1d8d7165bfe8df2c7f170698b
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2022-07-03 16:30:43 +0900

    Operator

---
+++ src/hijava/practice/Oper.java
@@ -0,0 +1,19 @@
+package hijava.practice;
+
+public class Oper {
+
+	public static void main(String[] args) {
+		int i = 0;
+		i += 1;
+		i *= 10;
+		System.out.println(i);
+		
+		int k = (i > 0) ? 100 : 1000;
+		System.out.println("k=" + k);
+		
+		i++; // i = i + 1
+		System.out.println(5 | 10);
+
+	}
+
+}
