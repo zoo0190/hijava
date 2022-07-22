b79d5492f8462e618e936b2f696c69658652e392
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2022-07-23 01:04:35 +0900

    Java 기초 17 - i/o 입출력

--- src/hijava/basic/StreamMain.java
+++ src/hijava/basic/StreamMain.java
@@ -1,14 +1,21 @@
 package hijava.basic;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class StreamMain {
+	private static final String FILE = "test.txt";
 	public static void main(String[] args) throws IOException {
 //		test1();
 //		test2();
@@ -16,9 +23,70 @@ public class StreamMain {
 //		test3();
 //		test4();
 //		test5();
-		test6();
+//		test6();
+		writeFile("세종대왕 한글 123");
+		readFile();
 	}
 	
+	private static final String CHARSET = "UTF-8";
+	private static void writeFile(String content) {
+		File file = new File(FILE);
+		try (FileOutputStream fos = new FileOutputStream(file)) {
+			OutputStreamWriter osw = new OutputStreamWriter(fos, CHARSET);
+			BufferedWriter bw = new BufferedWriter(osw);
+			bw.write(content);
+			bw.flush();
+			bw.close();
+			
+			System.out.println("Write OK : " + file.getAbsolutePath());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void readFile() {
+		try (FileInputStream fis = new FileInputStream(FILE)) {
+			InputStreamReader isr = new InputStreamReader(fis, CHARSET);
+			BufferedReader br = new BufferedReader(isr);
+			String data = null;
+			while((data = br.readLine()) != null) {
+				System.out.println(data);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void readFile0() {
+		try (FileInputStream fis = new FileInputStream(FILE)) {
+			int data = 0;
+//			byte[] buffer = new byte[10];
+			while((data = fis.read()) != -1)
+				System.out.println((char)data);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void writeFile0(String content) {
+		File file = new File(FILE);
+		try (FileOutputStream fos = new FileOutputStream(file)) {
+			fos.write(content.getBytes());
+			
+			System.out.println("Write OK : " + file.getAbsolutePath());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	// 좋은 코드!!
 	private static void test6() {
 		try(FileInputStream fis = new FileInputStream("test.txt")) {
