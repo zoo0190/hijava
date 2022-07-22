25318888d4d10addf8e46bf7a3bc7faccc3bf070
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2022-07-22 23:37:36 +0900

    Java 기초 16 - Exception 예외처리

--- src/hijava/basic/StreamMain.java
+++ src/hijava/basic/StreamMain.java
@@ -1,16 +1,69 @@
 package hijava.basic;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class StreamMain {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 //		test1();
 //		test2();
-		tryThis();
+//		tryThis();
+//		test3();
+//		test4();
+//		test5();
+		test6();
+	}
+	
+	// 좋은 코드!!
+	private static void test6() {
+		try(FileInputStream fis = new FileInputStream("test.txt")) {
+			System.out.println("hihihihihihi");
+		} catch (FileNotFoundException e) {
+			System.out.println("test.txt파일이 없어요!!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void test5() throws IOException {
+		FileInputStream fis = new FileInputStream("test.txt");
+		fis.close();
+	}
+
+	// 개선된 버전
+	private static void test4() {
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream("test.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("text.txt파일이 없어요!!" + e.getMessage());
+		} finally {
+			try {
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void test3() {
+		try {
+			FileInputStream fis = new FileInputStream("test.txt");
+			fis.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("text.txt파일이 없어요!!" + e.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			
+		}
 	}
 
 	private static void tryThis() {
